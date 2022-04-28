eeee3d08f033ed8075a67f82c31add04c72a1a5f
paco-portada <paco.portada@gmail.com>
2022-04-28 18:51:57 +0000

    Cambios

--- Model/oferta.sql
+++ Model/oferta.sql
@@ -19,7 +19,9 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `pizzeria`
 --
+create database pizzeria;
 
+use pizzeria;
 -- --------------------------------------------------------
 
 --
@@ -32,16 +34,16 @@ CREATE TABLE IF NOT EXISTS `oferta` (
   `imagen` varchar(100) COLLATE utf8_bin NOT NULL,
   `descripcion` varchar(500) COLLATE utf8_bin NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=17 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci AUTO_INCREMENT=4;
 
 --
 -- Volcado de datos para la tabla `oferta`
 --
 
 INSERT INTO `oferta` (`id`, `titulo`, `imagen`, `descripcion`) VALUES
-(3, 'Bebida gratis pidiendo dos pizzas', 'pizza3.jpg', 'Pidiendo dos pizzas de cualquier tipo te regalamos dos bebidas (no incluye bebidas alcohÃ³licas de alta graduaciÃ³n).		'),
-(15, 'Oferta de la semana: Funghi a mitad de precio', 'pizza1.jpg', 'Durante esta semana todas las pizzas funghi de tamaÃ±o mediano y familiar estÃ¡n a mitad de precio. Â¡Viva el champiÃ±Ã³n!'),
-(16, 'Albahaca gratis en cualquier pizza', 'pizza2.jpg', 'Al encargar cualquier pizza puedes pedir que le aÃ±adamos albahaca Â¡totalmente gratis!');
+(1, 'Bebida gratis pidiendo dos pizzas', 'pizza3.jpg', 'Pidiendo dos pizzas de cualquier tipo te regalamos dos bebidas (no incluye bebidas alcohólicas de alta graduación).		'),
+(2, 'Oferta de la semana: Funghi a mitad de precio', 'pizza1.jpg', 'Durante esta semana todas las pizzas funghi de tamaño mediano y familiar estÃ¡n a mitad de precio. ¡Viva el champiñón!'),
+(3, 'Albahaca gratis en cualquier pizza', 'pizza2.jpg', 'Al encargar cualquier pizza puedes pedir que le añadamos albahaca ¡totalmente gratis!');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
